1bda40063a7d5316581c03122ceeb3d1cbcb8bc6
Marc Robotham <marcrobotham@Marcs-Mac-mini.local>
2024-01-01 22:26:38 -0500

    finish header responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,29 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import compress from "astro-compress"
 
-import preact from "@astrojs/preact";
+import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), compress(), preact()]
-});
+	compressHTML: true,
+	integrations: [
+		mdx(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		compress(),
+		preact(),
+	],
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: ` 
+				@use "../../../node_modules/astro-scroll-observer/use-scroll-observer.scss" as *;
+			  `,
+			},
+		},
+	},
+})
